ab52fdec29139bbdeb2cf7796f1e486b1ee5b658
Snyp <gabrielmarques.mg@gmail.com>
2019-04-16 12:47:10 -0300

    [Add]_DTO
    Adicionado DTO's de Agenda, Aluno, Avaliacao, Exercicio e Professor;

--- Sistema/Services/src/main/java/DTO/AlunoDTO.java
+++ Sistema/Services/src/main/java/DTO/AlunoDTO.java
@@ -1,9 +1,26 @@
 package DTO;
 
+import java.util.Date;
 import lombok.Data;
 
 @Data
 public class AlunoDTO {
+    private int IdAluno;
+    private int IdFicha;
+    private int IdAvaliacao;
+    private int Permissao;
+    private String Matricula;
+    private String Nome; 
     private String Email;
-    private String Senha;    
+    private String Senha;
+    private String Rua;
+    private String Bairro;
+    private Date DataNascimento;
+    private int Numero;
+    private int Complemento;
+    private int Cep;
+    private int Cpf;
+    private int Telefone;
+    private int Celular;
+    private int Sexo;
 }
--- Sistema/Services/src/main/java/DTO/ProfessorDTO.java
+++ Sistema/Services/src/main/java/DTO/ProfessorDTO.java
@@ -1,19 +1,27 @@
 package DTO;
 
+import java.util.Date;
 import lombok.Data;
 
 @Data
 public class ProfessorDTO {
     private int IdProfessor;
-    private int IdUnidade;  
+    private int IdUnidade;
+    private int IdAgenda;
+    private int Permissao;
+    private String Cref;
+    private String Nome; 
+    private String Email;
+    private String Senha;
     private String Rua;
     private String Bairro;
+    private Date DataNascimento;
+    private Date DataAdmissao;
     private int Numero;
     private int Complemento;
     private int Cep;
     private int Cpf;
-    private String Cref;
-    private String Nome;
-    private String Email;
-    private String Senha;    
+    private int Telefone;
+    private int Celular;
+    private int Sexo;
 }
