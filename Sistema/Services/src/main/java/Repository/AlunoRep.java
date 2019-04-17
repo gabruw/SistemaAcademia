c4179e67605194d0f7b5bde2efc40ea8443b28a1
Snyp <gabrielmarques.mg@gmail.com>
2019-04-17 11:16:35 -0300

    [Edit]_Estrutura

--- Sistema/Services/src/main/java/Repository/AlunoRep.java
+++ Sistema/Services/src/main/java/Repository/AlunoRep.java
@@ -4,5 +4,15 @@ import Model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRep extends JpaRepository<Aluno, Integer>{
+    public Aluno findByIdAluno(int IdAluno);
+    
+    public Aluno findByMatricula(String Matricula);
+    
+    public Aluno findByNome(String Nome);
+    
+    public Aluno findBySexo(int Sexo);
+    
+    public Aluno findByCpf(int Cpf);
+    
     public Aluno findByEmailAndSenha(String Email, String Senha);
 }
--- Sistema/Services/src/main/java/Repository/ProfessorRep.java
+++ Sistema/Services/src/main/java/Repository/ProfessorRep.java
@@ -1,8 +1,21 @@
 package Repository;
 
 import Model.Professor;
+import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfessorRep extends JpaRepository<Professor, Integer>{
+    public Professor findByIdProfessor(int IdProfessor);
+    
+    public Professor findByCref(String Cref);
+    
+    public Professor findByNome(String Nome);
+    
+    public Professor findByCpf(int Cpf);
+    
+    public Professor findByDataAdmissao(Date DataAdmissao);
+    
+    public Professor findBySexo(int Sexo);
+    
     public Professor findByEmailAndSenha(String Email, String Senha);
 }
