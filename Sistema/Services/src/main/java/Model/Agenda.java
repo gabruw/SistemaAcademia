51929e46caeadea181cb31653e79b946b2091360
IgorCooli <igorcooli@gmail.com>
2019-04-18 18:03:20 -0300

    Criando Model Agenda

--- Sistema/Services/src/main/java/Model/Agenda.java
+++ Sistema/Services/src/main/java/Model/Agenda.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
-/**
- *
- * @author SHU Aeronáutica 01
- */
-public class Agenda {
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+
+@Entity
+@Table(name = "agenda")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Agenda.findAll", query = "SELECT u FROM Agenda u"),
+    @NamedQuery(name = "Aluno.findByIdAgenda", query = "SELECT u FROM Agenda u WHERE u.IdAgenda = :IdAgenda"),
+    @NamedQuery(name = "Agenda.findByIdProfessor", query = "SELECT u FROM Agenda u WHERE u.IdProfessor = :IdProfessor"),
+    @NamedQuery(name = "Agenda.findByIdAluno", query = "SELECT u FROM Agenda u WHERE u.IdAluno = :IdAluno")
+})
+
+
+@Data
+public class Agenda implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "IdAgenda")
+    private int IdAgenda;
+    
+    @Basic(optional = false)
+    @ManyToOne
+    @JoinColumn(name = "IdProfessor")
+    private Avaliacao IdProfessor;
+    
+    @Basic(optional = false)
+    @ManyToOne
+    @JoinColumn(name = "IdAluno")
+    private Avaliacao IdAluno;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 1)
+    @Column(name = "Data")
+    private int Data;
     
 }
--- Sistema/Services/src/main/java/Model/Aparelho.java
+++ Sistema/Services/src/main/java/Model/Aparelho.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
-/**
- *
- * @author SHU Aeronáutica 01
- */
-public class Aparelho {
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "aparelho")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Aparelho.findAll", query = "SELECT u FROM Aparelho u"),
+    @NamedQuery(name = "Aparelho.findByIdAparelho", query = "SELECT u FROM Aparelho u WHERE u.IdAparelho = :IdAparelho"),
+    @NamedQuery(name = "Aparelho.findByIdAparelho", query = "SELECT u FROM Aparelho u WHRE u.Nome = :Nome")
+    
+})
+
+@Data
+public class Aparelho implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
     
 }
