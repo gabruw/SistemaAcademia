9dca741f64e7e065e1230204dfbb4b98e5578672
Snyp <36605990+gabruw@users.noreply.github.com>
2019-06-05 21:35:13 -0300

    Update ProfessorRes.java

--- Sistema/Services/src/main/java/Resource/ProfessorRes.java
+++ Sistema/Services/src/main/java/Resource/ProfessorRes.java
@@ -23,11 +23,55 @@ public class ProfessorRes {
     EntityManager EntityM;
     
     @PostMapping("/login")
-    public Professor getAllUsers(@RequestBody ProfessorDTO professor) {
+    public Professor getProfessorByEmail(@RequestBody ProfessorDTO professor) {
         try {
-            Query query = EntityM.createQuery("SELECT u FROM Professor u WHERE u.Email = :email " + "and u.Senha = :senha");
+            Query query = EntityM.createQuery("SELECT u FROM Aluno u WHERE u.email = :email");
+            query.setParameter("email", perofessor.getEmail());
+
+            return (Professor) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+    
+    public Professor getProfessorByEmailAndSenha(@RequestBody ProfessorDTO professor) {
+        try {
+            Query query = EntityM.createQuery("SELECT u FROM Professor u WHERE u.Email = :email AND u.Senha = :senha");
             query.setParameter("email", professor.getEmail());
             query.setParameter("senha", professor.getSenha());
+
+            return (Professor) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+    
+    @PostMapping("/search")
+    public Professor getAllUsers(@RequestBody ProfessorDTO professor) {
+        try {
+            Query query = EntityM.createQuery("SELECT * FROM Professor");
+            
+            return (Professor) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+    
+    public Professor getProfessorById(@RequestBody ProfessorDTO professor) {
+        try {
+            Query query = EntityM.createQuery("SELECT u FROM Professor u WHERE u.idProfessor = :idProfessor");
+            query.setParameter("idProfessor", professor.getIdProfessor());
+
+            return (Professor) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+    
+    public Professor getProfessorByCpf(@RequestBody ProfessorDTO professor) {
+        try {
+            Query query = EntityM.createQuery("SELECT u FROM Professor u WHERE u.cpf = :cpf");
+            query.setParameter("cpf", professor.getCpf());
 
             return (Professor) query.getSingleResult();
         } catch (NoResultException e) {
